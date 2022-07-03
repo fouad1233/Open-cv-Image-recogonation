4ffee0bbd6d9cdbcb96a34be1647bb33c2c50da6
Fouad Aladhami <fouadiadhami@gmail.com>
2022-07-03 16:32:35 +0300

    main

---
+++ faces_train.py
@@ -0,0 +1,54 @@
+#pylint:disable=no-member
+
+import os
+import cv2 as cv
+import numpy as np
+
+DIR = r'/Users/fuad/Desktop/Python workspace/Opencv_face_recognation/Resources/Faces/train'
+people = []
+for i in os.listdir(DIR):
+    people.append(i) 
+#print(people)
+# haar_cascade = cv.CascadeClassifier('haar_face.xml') 
+haar_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+features = []
+labels = []
+
+def create_train():
+    for person in people:
+        path = os.path.join(DIR, person)
+        label = people.index(person)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path,img)
+
+            img_array = cv.imread(img_path)
+            if img_array is None:
+                continue 
+                
+            gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
+
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
+            for (x,y,w,h) in faces_rect:
+                faces_roi = gray[y:y+h, x:x+w]
+                features.append(faces_roi)
+                labels.append(label)
+
+create_train()
+# print(f'Length of the feautures  = {len(features)}')
+# print(f'Length of the labels  = {len(labels)}')
+print('Training done ---------------')
+
+features = np.array(features, dtype='object')
+labels = np.array(labels)
+
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+
+# # Train the Recognizer on the features list and the labels list
+face_recognizer.train(features,labels)
+
+face_recognizer.save('face_trained.yml')
+np.save('features.npy', features)
+np.save('labels.npy', labels)
