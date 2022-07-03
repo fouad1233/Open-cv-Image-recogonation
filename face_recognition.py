d6f4ff119a7abfe32ad720287c4b67fa0cf5a537
Fouad Aladhami <fouadiadhami@gmail.com>
2022-07-03 17:44:52 +0300

    Change the text place And i print faces_rect for testing

--- face_recognition.py
+++ face_recognition.py
@@ -15,14 +15,14 @@ labels = np.load('labels.npy')
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read('face_trained.yml')
 
-img = cv.imread(r'/Users/fuad/Desktop/Python workspace/Opencv_face_recognation/Resources/Photoes for test/2.jpeg')
+img = cv.imread(r'/Users/fuad/Desktop/Python workspace/Opencv_face_recognation/Resources/Photoes for test/Friends.jpeg')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Person', gray)
 
 # Detect the face in the image
 faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
-
+print(faces_rect)
 for (x,y,w,h) in faces_rect:
     faces_roi = gray[y:y+h,x:x+w]
 
@@ -30,7 +30,7 @@ for (x,y,w,h) in faces_rect:
     print("Label = " , label)
     print(f'Label = {people[label]} with a confidence of {confidence}')
 
-    cv.putText(img, str(people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=2)
+    cv.putText(img, str(people[label]), (x+w,y+h), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=2)
     cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
 
 cv.imshow('Detected Face', img)
